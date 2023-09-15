6882b86dbc6781773b43f11aeaf93ba54a9dac9a
Rentoo <dreamYiZ@outlook.com>
2023-09-16 06:55:26 +0800

    import remove

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,9 @@
-import time
 import sqlite3
-import redis
 from flask import Flask, jsonify, send_from_directory
 from init_db import init_db
 import _db
-from pathlib import Path
-import pprint
 import os
-import glob
 import urllib
-from werkzeug.utils import secure_filename
 import util
 
 
@@ -17,31 +11,18 @@ MUSIC_FOLDER = '/music'
 
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
 app.config['MUSIC_FOLDER'] = MUSIC_FOLDER
 
 
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
 
 
 
 
 @app.route('/')
 def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been --312? seen {} times.\n'.format(count)
-
+    return 'music management'
 
 
 
